ca3c9edf4cded23237053f197df57553847f90fd
Bjorn Reese <breese@users.sourceforge.net>
2019-08-18 15:22:25 +0200

    Rewrote is_nothrow_swappable trait to compile with Visual Studio

--- include/trial/circular/detail/type_traits.hpp
+++ include/trial/circular/detail/type_traits.hpp
@@ -20,26 +20,29 @@ namespace circular
 namespace detail
 {
 
-#if __cplusplus >= 201703L
+#if __cpp_lib_is_swappable >= 201603L
 
 template <typename T>
 using is_nothrow_swappable = std::is_nothrow_swappable<T>;
 
 #else
 
-template <typename T, typename = void>
-struct is_nothrow_swappable
-    : public std::false_type
-{
-};
-
 // See P0185R1
 
+using std::swap;
+
+struct is_nothrow_swappable_tester
+{
+    template <typename T>
+    static auto test(int) -> std::integral_constant<bool, noexcept(swap(std::declval<T&>(), std::declval<T&>()))>;
+
+    template <typename T>
+    static std::false_type test(...);
+};
+
 template <typename T>
-struct is_nothrow_swappable<T,
-                            std::enable_if<std::is_nothrow_move_constructible<T>::value &&
-                                           noexcept(std::swap(std::declval<T&>(), std::declval<T&>()))>>
-    : public std::true_type
+struct is_nothrow_swappable
+    : decltype(is_nothrow_swappable_tester::test<T>(0))
 {
 };
 
